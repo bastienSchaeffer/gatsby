1158f764006325e11f01ede7635c472deb4a769a
Bastien <bast.schaeffer@gmail.com>
2019-10-06 15:39:32 +0100

    add netlify header

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,8 @@ module.exports = {
 
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
